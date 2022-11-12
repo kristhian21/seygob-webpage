12f0adcf1faf3139b3f8ba967cfd2d68a520503f
Kristhian David Segura <kristhian.segura@mail.escuelaing.edu.co>
2022-11-11 23:12:42 -0500

    NavBar finished

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,10 +1,10 @@
 export default function Hero() {
   return (
-    <div className="flex items-center justify-center h-screen mb-12 bg-fixed bg-center bg-cover custom-image">
+    <div className="flex items-center justify-center h-screen mb-12 custom-image">
       <div />
       <div className="grid grid-cols-2">
         <div>
-          <h1 className="text-5xl font-black bg-clip-text text-transparent bg-gradient-to-r from-violet-700 to-sky-400">
+          <h1 className="text-5xl font-black text-amber-400">
             SOLUCIONES EMPRESARIALES Y DE GOBIERNO SEYGOB SAS
           </h1>
           <p className="font-extrabold text-xl">
--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,32 +1,56 @@
 import Link from "next/link";
-import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
-import React, { useState } from "react";
 import Image from "next/image";
+import React, { useState, useEffect } from "react";
+import { AiOutlineMenu, AiOutlineClose } from "react-icons/ai";
 
 export default function NavBar() {
   const [nav, setNav] = useState(false);
+  const [color, setColor] = useState("transparent");
+  const [textColor, setTextColor] = useState("white");
+  const [logo, setLogo] = useState("/white-logo.png");
 
   const handleNav = () => {
     setNav(!nav);
   };
 
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 90) {
+        setColor("#ffffff");
+        setTextColor("#000000");
+        setLogo("/logo.png");
+      } else {
+        setColor("transparent");
+        setTextColor("#ffffff");
+        setLogo("/white-logo.png");
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
-    <div className="fixed left-0 top-0 w-full z-10 ease-in duration-300 bg-slate-50 border-2 border-b-slate-300/75 shadow-md">
-      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 ">
+    <div
+      style={{ backgroundColor: `${color}` }}
+      className="fixed left-0 top-0 w-full z-10 ease-in duration-300"
+    >
+      <div className="max-w-[1240px] m-auto flex justify-between items-center p-4 text-white">
         <Link href="/" className="flex items-center">
-          <Image src="/logo.png" alt="Logo image" width={50} height={50} />
-          <h1 className="text-3xl tracking-widest text-slate-500 ml-6">
+          <Image src={`${logo}`} alt="Logo image" width={50} height={50} />
+          <h1
+            style={{ color: `${textColor}` }}
+            className="font-bold text-4xl ml-6"
+          >
             SEYGOB
           </h1>
         </Link>
-        <ul className="hidden sm:flex  text-slate-500">
-          <li className="p-4 text-l hover:text-blue-600 font-semibold">
-            <Link href="/">Acerca de</Link>
+        <ul style={{ color: `${textColor}` }} className="hidden sm:flex">
+          <li className="p-4">
+            <Link href="/">Nuestro trabajo</Link>
           </li>
-          <li className="p-4 text-l hover:text-blue-600 font-semibold">
+          <li className="p-4">
             <Link href="/soluciones">Soluciones</Link>
           </li>
-          <li className="p-4 text-l hover:text-blue-600 font-semibold">
+          <li className="p-4">
             <Link href="/contacto">Contacto</Link>
           </li>
         </ul>
@@ -34,35 +58,35 @@ export default function NavBar() {
         {/* Mobile Button */}
         <div onClick={handleNav} className="block sm:hidden z-10">
           {nav ? (
-            <AiOutlineClose size={20} className="text-blue-600" />
+            <AiOutlineClose size={20} style={{ color: "black" }} />
           ) : (
-            <AiOutlineMenu size={20} className="text-blue-600" />
+            <AiOutlineMenu size={20} style={{ color: `${textColor}` }} />
           )}
         </div>
         {/* Mobile Menu */}
         <div
           className={
             nav
-              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-slate-50 text-center"
-              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-slate-50 text-center"
+              ? "sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300 text-black"
+              : "sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-white text-center ease-in duration-300 text-black"
           }
         >
-          <ul className="text-slate-500">
+          <ul>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl hover:text-blue-600"
+              className="p-4 text-4xl hover:text-gray-500"
             >
-              <Link href="/">Acerca de</Link>
+              <Link href="/">Nuestro trabajo</Link>
             </li>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl hover:text-blue-600"
+              className="p-4 text-4xl hover:text-gray-500"
             >
               <Link href="/soluciones">Soluciones</Link>
             </li>
             <li
               onClick={handleNav}
-              className="p-4 text-4xl hover:text-blue-600"
+              className="p-4 text-4xl hover:text-gray-500"
             >
               <Link href="/contacto">Contacto</Link>
             </li>
